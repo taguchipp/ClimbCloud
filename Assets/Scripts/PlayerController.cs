63bb3135c4701f0aa495eddcc90f3a1b02a53ef7
ttaguchi <ozaki6626pyahoo.co.jp>
2024-03-07 11:47:17 +0900

    左右移動

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     public float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +18,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //ジャンプ
         if(Input.GetKeyDown(KeyCode.Space)){
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }    
+        //左右移動
+        int key = 0;
+        if(Input.GetKey(KeyCode.RightArrow)) key =1;
+        if(Input.GetKey(KeyCode.LeftArrow)) key =-1;
+        //プレイヤーの速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        //スピード制限
+        if(speedx <this.maxWalkSpeed){
+            this.rigid2D.AddForce(transform.right *key * this.walkForce);
+        }
+        //動く方向に応じて反転
+        if(key != 0){
+            transform.localScale = new Vector3(key,1,1);
+        }
     }
 }
