5bd2da90c46f42c1724a4367feedc114ee985507
ttaguchi <ozaki6626pyahoo.co.jp>
2024-03-07 13:02:40 +0900

    移動アニメーション

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
+    Animator animator;
     public float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour
     {
         Application.targetFrameRate = 60;
         this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -37,5 +39,7 @@ public class PlayerController : MonoBehaviour
         if(key != 0){
             transform.localScale = new Vector3(key,1,1);
         }
+        //プレイヤーの速度に応じてアニメーション速度を変える
+        this.animator.speed = speedx * 2.0f;
     }
 }
