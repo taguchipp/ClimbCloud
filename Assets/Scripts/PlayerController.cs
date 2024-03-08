9fff67cb077ba02d698d307dd5df00288d826139
ttaguchi <ozaki6626pyahoo.co.jp>
2024-03-08 11:22:51 +0900

    ジャンプアニメーション追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,6 +24,7 @@ public class PlayerController : MonoBehaviour
         //ジャンプ
         if(Input.GetKeyDown(KeyCode.Space) && 
             this.rigid2D.velocity.y == 0){
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }    
         //左右移動
@@ -42,7 +43,11 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(key,1,1);
         }
         //プレイヤーの速度に応じてアニメーション速度を変える
-        this.animator.speed = speedx * 2.0f;
+        if(this.rigid2D.velocity.y == 0){
+            this.animator.speed = speedx * 2.0f;
+        }else{
+            this.animator.speed = 1.0f;
+        }
         //画面外に出たら最初から
         if(transform.position.y < -10){
             SceneManager.LoadScene("GameScene");
